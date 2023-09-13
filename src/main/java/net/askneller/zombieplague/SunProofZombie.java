e526bbce3e07336e17b895571f39a4c65cea3664
Adam Kneller <a_kneller@yahoo.com.au>
2023-09-13 22:40:07 +1000

    Cleanup

--- src/main/java/net/askneller/zombieplague/SunProofZombie.java
+++ src/main/java/net/askneller/zombieplague/SunProofZombie.java
@@ -11,22 +11,9 @@ import org.slf4j.Logger;
 
 public class SunProofZombie extends Zombie {
 
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     public SunProofZombie(EntityType<? extends Zombie> p_34271_, Level p_34272_) {
         super(p_34271_, p_34272_);
     }
-
-//    public static AttributeSupplier.Builder createAttributes() {
-//        return Monster.createMonsterAttributes()
-//                .add(Attributes.FOLLOW_RANGE, 35.0D)
-//                .add(Attributes.MOVEMENT_SPEED, (double)0.23F)
-//                .add(Attributes.ATTACK_DAMAGE, 3.0D)
-//                .add(Attributes.ARMOR, 2.0D)
-//                // Don't call reinforcements
-//                .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE)
-//                ;
-//    }
 
     @Override
     protected void randomizeReinforcementsChance() {
