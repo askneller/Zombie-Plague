a8455059084d8e83f4494745ac65c887fb957dba
Adam Kneller <a_kneller@yahoo.com.au>
2023-09-20 20:26:41 +1000

    Add sulfur recipe, update gunpowder

--- src/main/java/net/askneller/zombieplague/ZombiePlague.java
+++ src/main/java/net/askneller/zombieplague/ZombiePlague.java
@@ -49,15 +49,17 @@ public class ZombiePlague {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
     public static final RegistryObject<Item> PLANT_FIBRE = ITEMS.register("plant_fibre", () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> BLUNDERBUSS = ITEMS.register("blunderbuss",
             () -> new BlunderbussItem(new Item.Properties().stacksTo(1).durability(465)));
     public static final RegistryObject<Item> BLUNDERBUSS_SHOT = ITEMS.register("blunderbuss_shot", () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> MUSKET = ITEMS.register("musket",
             () -> new MusketItem(new Item.Properties().stacksTo(1).durability(465)));
     public static final RegistryObject<Item> MUSKET_BALL = ITEMS.register("musket_ball", () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> RAW_NITRE = ITEMS.register("raw_nitre", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> SALTPETRE = ITEMS.register("saltpetre", () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Block> NITRE = BLOCKS.register("nitre", () -> new Block(
             BlockBehaviour.Properties.of()
                     .mapColor(MapColor.STONE)
@@ -74,6 +76,7 @@ public class ZombiePlague {
                     .strength(1.0F, 3.0F)));
     public static final RegistryObject<Item> SULFUR_ORE_BLOCK_ITEM = ITEMS.register("sulfur_ore", () -> new BlockItem(SULFUR_ORE.get(), new Item.Properties()));
     public static final RegistryObject<Item> RAW_SULFUR = ITEMS.register("raw_sulfur", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> REFINED_SULFUR = ITEMS.register("refined_sulfur", () -> new Item(new Item.Properties()));
 
     public ZombiePlague() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
