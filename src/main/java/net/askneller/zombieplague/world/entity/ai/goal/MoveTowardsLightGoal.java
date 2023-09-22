05e1c978aa2f0d74ab9a9c596b636742a71a874c
Adam Kneller <a_kneller@yahoo.com.au>
2023-09-22 18:19:15 +1000

    Cleanup

--- src/main/java/net/askneller/zombieplague/world/entity/ai/goal/MoveTowardsLightGoal.java
+++ src/main/java/net/askneller/zombieplague/world/entity/ai/goal/MoveTowardsLightGoal.java
@@ -3,10 +3,9 @@ package net.askneller.zombieplague.world.entity.ai.goal;
 import com.mojang.logging.LogUtils;
 import net.askneller.zombieplague.entity.LightSourceMarkerEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
@@ -17,8 +16,6 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 
-import javax.annotation.Nullable;
-import java.util.EnumSet;
 import java.util.List;
 
 public class MoveTowardsLightGoal extends RandomStrollGoal {
@@ -31,80 +28,26 @@ public class MoveTowardsLightGoal extends RandomStrollGoal {
     public static final double MIN_DISTANCE = 5.0;
     public static final double MAX_LIGHT_SOURCE_DISTANCE = 50.0;
     public static final int CHECK_AROUND_DISTANCE = 3;
-//    protected final Mob mob;
-//    @Nullable
-//    protected Entity lookAt;
-//    protected final float lookDistance;
-//    private int lookTime;
+
     protected final float probability = DEFAULT_PROBABILITY;
-//    private final boolean onlyHorizontal;
-//    protected final Class<? extends LivingEntity> lookAtType;
-//    protected final TargetingConditions lookAtContext;
 
     protected BlockPos destination;
-
-//    public MoveTowardsLightGoal(Mob p_25520_, Class<? extends LivingEntity> p_25521_, float p_25522_) {
-//        this(p_25520_, p_25521_, p_25522_, DEFAULT_PROBABILITY);
-//    }
-
-//    public MoveTowardsLightGoal(Mob p_25524_, Class<? extends LivingEntity> p_25525_, float p_25526_, float p_25527_) {
-//        this(p_25524_, p_25525_, p_25526_, p_25527_, false);
-//    }
-
-//    public MoveTowardsLightGoal(Mob mob, Class<? extends LivingEntity> p_148119_, float lookDistance, float probability, boolean p_148122_) {
-////        logger.info("Created behaviour");
-//        super(mob, 1.0);
-//        this.mob = mob;
-//        this.lookAtType = p_148119_;
-//        this.lookDistance = lookDistance;
-//        this.probability = probability;
-//        this.onlyHorizontal = p_148122_;
-//        this.setFlags(EnumSet.of(Goal.Flag.LOOK));
-//        if (p_148119_ == Player.class) {
-//            this.lookAtContext = TargetingConditions.forNonCombat().range((double)lookDistance).selector((p_25531_) -> {
-//                return EntitySelector.notRiding(mob).test(p_25531_);
-//            });
-//        } else {
-//            this.lookAtContext = TargetingConditions.forNonCombat().range((double)lookDistance);
-//        }
-//
-//    }
-
-    public MoveTowardsLightGoal(PathfinderMob mob, double speedModifier) {
-        super(mob, speedModifier);
-    }
 
     public MoveTowardsLightGoal(PathfinderMob mob, double speedModifier, int interval) {
         super(mob, speedModifier, interval);
     }
 
-    public MoveTowardsLightGoal(PathfinderMob mob, double speedModifier, int interval, boolean checkNoActionTime) {
-        super(mob, speedModifier, interval, checkNoActionTime);
-    }
-
     public boolean canUse() {
+        // TODO implement an interval between selection
         if (this.mob.getRandom().nextFloat() >= this.probability) {
 //            logger.info("Below probability");
             return false;
         } else {
+            // todo remove after debugging
             Player nearestPlayer = this.mob.level().getNearestPlayer(this.mob, 15.0);
             if (nearestPlayer == null) {
                 return false;
             }
-//            if (this.mob.getTarget() != null) {
-//                this.lookAt = this.mob.getTarget();
-//            }
-//
-//            if (this.lookAtType == Player.class) {
-//                this.lookAt = this.mob.level().getNearestPlayer(this.lookAtContext, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
-//            } else {
-//                this.lookAt = this.mob.level().getNearestEntity(this.mob.level().getEntitiesOfClass(this.lookAtType, this.mob.getBoundingBox().inflate((double)this.lookDistance, 3.0D, (double)this.lookDistance), (p_148124_) -> {
-//                    return true;
-//                }), this.lookAtContext, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
-//            }
-//
-//            return this.lookAt != null;
-//            return true;
 
             // it has to be dark enough to see the light
             int rawBrightness = this.mob.level().getLightEngine().getRawBrightness(this.mob.blockPosition(), 0);
@@ -230,15 +173,6 @@ public class MoveTowardsLightGoal extends RandomStrollGoal {
     }
 
     public boolean canContinueToUse() {
-//        if (!this.lookAt.isAlive()) {
-//            return false;
-//        } else if (this.mob.distanceToSqr(this.lookAt) > (double)(this.lookDistance * this.lookDistance)) {
-//            return false;
-//        } else {
-//            return this.lookTime > 0;
-//        }
-        // TODO create empty entity to place with torches or campfires
-        // TODO see Marker extends Entity
         if (this.destination == null) return false;
 
         // is the light still there
@@ -267,7 +201,7 @@ public class MoveTowardsLightGoal extends RandomStrollGoal {
             return !anyMatch;
 
         }
-        return !entities.isEmpty();
+        return false;
     }
 
     private void setDestination(BlockPos pos) {
@@ -281,23 +215,8 @@ public class MoveTowardsLightGoal extends RandomStrollGoal {
     public void start() {
         if (printDebug(this.mob.level())) logger.info("Starting go to {}", this.destination);
         super.start();
-//        this.lookTime = this.adjustedTickDelay(40 + this.mob.getRandom().nextInt(40));
         if (printDebug(this.mob.level())) logger.info("nav target {}", this.mob.getNavigation().getTargetPos());
     }
-
-    public void stop() {
-        super.stop();
-//        this.lookAt = null;
-    }
-
-//    public void tick() {
-//        if (this.lookAt.isAlive()) {
-//            double d0 = this.onlyHorizontal ? this.mob.getEyeY() : this.lookAt.getEyeY();
-//            this.mob.getLookControl().setLookAt(this.lookAt.getX(), d0, this.lookAt.getZ());
-//            --this.lookTime;
-//        }
-//        logger.info("Tick");
-//    }
 
     public boolean hasLineOfSight(Entity from, BlockPos to, Level toLevel, Entity p_147185_) {
         if (toLevel != from.level()) {
